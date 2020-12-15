15e4f0680545397c9de7c9c633f9ea5966cc2752
yqq <youngqqcn@163.com>
2020-12-15 16:07:35 +0800

    add docs and refactor directories

---
+++ htdfsdk/__init__.py
@@ -0,0 +1,5 @@
+name = "htdfsdk"
+
+from htdfsdk.htdf_address_generate import generate_htdf_address
+
+__all__ = ["generate_htdf_address"]
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='htdfsdk',
-    version='0.0.1',
+    version='0.0.3',
     description='htdf sdk',
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -28,10 +28,10 @@ setuptools.setup(
         'License :: OSI Approved :: MIT License',  # 模块的license
 
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-    ]
+        'Programming Language :: Python :: 3.8',
+    ],
+    python_requires='>=3.6',
 
 )

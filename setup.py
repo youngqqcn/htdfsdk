2ad4f48c36109872365d88cc7df191f798cbc739
yqq <youngqqcn@163.com>
2020-12-16 17:13:37 +0800

    Bump version: 0.0.8 → 0.0.9

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='htdfsdk',
-    version='0.0.8',
+    version='0.0.9',
     description='htdf sdk',
     long_description=long_description,
     long_description_content_type="text/markdown",

c22f213a967c8233bb6ccfb01bf148112efd44db
yqq <youngqqcn@gmail.com>
2021-08-16 16:55:43 +0800

    Bump version: 1.0.4 → 1.0.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='htdfsdk',
-    version='1.0.4',
+    version='1.0.5',
     description='htdf python sdk',
     long_description=long_description,
     long_description_content_type="text/markdown",
